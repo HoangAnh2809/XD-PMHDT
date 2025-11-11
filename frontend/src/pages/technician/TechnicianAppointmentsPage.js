5a445a436d01f2159f014de000f4a3a1d2ba26b1
GiaBao <baohg3978@ut.edu.vn>
2025-11-11 09:41:41 +0700

    add technician appointments page (booking list)

---
+++ frontend/src/pages/technician/TechnicianAppointmentsPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+export default function TechnicianAppointmentsPage() {
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <h1 style={{marginTop: '2rem'}}>Lịch làm việc</h1>
+        <div className="card" style={{marginTop: '2rem'}}>
+          <p>Trang lịch làm việc - Đang phát triển</p>
+        </div>
+      </div>
+    </div>
+  );
+}
